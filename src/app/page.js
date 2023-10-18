aee6383ac213f76ce2f64dbdd7436a6cd9e37d55
zhuangtianjie@cmii.chinamobile.com <zhuangtianjie@cmii.chinamobile.com>
2023-10-18 13:35:00 +0800

    20231018_v3

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      我的第一个serverless应用
+      我的第一个serverless应用 智慧陪护
     </main>
   );
 }
